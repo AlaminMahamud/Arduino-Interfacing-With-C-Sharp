fcce62b119a0e268193ace3736f8c73663f4480b
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-03-23 19:40:36 +0600

    Arduino Repository Added

---
+++ Arduino First interfacing/C# part/Arduino First interfacing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO.Ports;
+
+namespace Arduino_First_interfacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort workingPort = new SerialPort();
+            workingPort.BaudRate = 9600;
+            workingPort.PortName = "COM15";
+            workingPort.Open();
+
+            whil    e (true)
+            {
+                string mystring = workingPort.ReadLine();
+                Console.WriteLine(mystring);
+            }
+        }
+    }
+}
